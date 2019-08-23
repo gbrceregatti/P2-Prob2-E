94b90b4be5625fc02009ae920c21faca0a679790
Henrique Alexsander Eichstadt <heichstadt@furb.br>
2019-08-23 19:36:46 -0300

    Adicionando as classes ContaCOrrente e Operacao

---
+++ Problema2/src/problema2/ContaCorrente.java
@@ -0,0 +1,25 @@
+package problema2;
+
+public class ContaCorrente {
+    
+    private int numero;
+    private int agencia;
+    private Cliente cliente;
+    private double saldo;
+    
+    public void sacar(double valor){
+        
+    }
+    
+    public void depositar(double valor){
+        
+    }
+    
+    public void transferir(double valor, ContaCorrente contaDestino){
+        
+    }
+    
+    private void receberTransferencia(double valor, ContaCorrente origem){
+        
+    }
+}
---
+++ Problema2/src/problema2/Operacao.java
@@ -0,0 +1,11 @@
+package problema2;
+
+import java.util.Date;
+
+public class Operacao {
+    private double valor;
+    private double saldoAnterior;
+    private TipoOperacao tipo;
+    private ContaCorrente conta;
+    private Date data;
+}
